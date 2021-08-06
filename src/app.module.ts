74146e58dbfed95c49e96e20b39f3a0c87e171b1
TheIdanLapid <idanl22@gmail.com>
2021-08-06 08:15:20 +0300

    pizza

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { PizzaModule } from './pizza/pizza.module';
 
 @Module({
-  imports: [OrderModule],
+  imports: [OrderModule, PizzaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/pizza/pizza.model.ts
@@ -0,0 +1,5 @@
+export class Pizza {
+constructor(
+    public toppings: string[]
+) {}
+}
