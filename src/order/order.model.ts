34e18ff070acfe16aa3ac7aa86967a2f7bacba45
TheIdanLapid <idanl22@gmail.com>
2021-08-06 10:00:58 +0300

    deleted unnecessary entity

--- src/order/order.model.ts
+++ src/order/order.model.ts
@@ -3,21 +3,6 @@ import { Status } from "../status.model";
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  firstName: string;
-
-  @Column()
-  lastName: string;
-
-  @Column({ default: true })
-  isActive: boolean;
-}
-
-@Entity()
 export class Order {
     @PrimaryGeneratedColumn()
     public id: number;
