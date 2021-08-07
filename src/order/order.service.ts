d43acdf0ba9c1878ecc03163e586df0863dc7cdd
TheIdanLapid <idanl22@gmail.com>
2021-08-07 11:13:22 +0300

    working on order repo

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -6,7 +6,13 @@ import { Connection, ConnectionManager, Repository } from "typeorm";
 
 @Injectable()
 export class OrderService {
-constructor(private connection: Connection) {}
+    
+constructor(private connection: Connection) {
+    @InjectRepository(Order)
+    usersRepository: new Repository<Order>() {
+        
+    };
+}
 
 // const Database = require('better-sqlite3');
 // const db = new this.Database('foobar.db', { verbose: console.log });
