9a650e50db39114c6381ab752ad4c2d5aa417a4e
TheIdanLapid <eontorch@gmail.com>
2021-08-08 00:24:07 +0300

    Started working on kitchen logic - added test resource to check it
    Signed-off-by: TheIdanLapid <eontorch@gmail.com>

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from '../order.entity';
 
 @Injectable()
 export class AppService {
+    constructor(@InjectRepository(Order) private ordersRepository: Repository<Order>) {
+        
+    }
   getHello(): string {
     return 'Hello World!';
   }
--- src/inventory.ts
+++ src/inventory.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Ingredient } from './ingredient';
+import { Ingredients } from './topping';
 
 @Injectable()
 export class Inventory {
-    ingredients: Ingredient[];
+    ingredients: Ingredients[];
     decrease(ing: Ingredient): boolean {
         if (this.ingredients.includes(ing)) {
             this.ingredients.splice(this.ingredients.indexOf(ing, 1));
             return true;
         }
         return false;
+    }
+    setToppings(toppings: Ingredients[]) {
+        this.ingredients = toppings;
     }
 }
